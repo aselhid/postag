ddaeeb51256e28e41c79de16c93a22ad4c0f9acb
Tatag Aziz Prawiro <prawirotatag@gmail.com>
2019-03-07 11:07:05 +0700

    fixed smoothing probabilities

--- smoother.py
+++ smoother.py
@@ -1,6 +1,10 @@
 import json
 import io
 
+with io.open('training-data.json', 'r', encoding='utf-8-sig') as raw_training_data:
+    json_training = json.load(raw_training_data)
+    types = json_training['corpus']
+
 with io.open('training-data-2.json', 'r', encoding='utf-8-sig') as raw_training_data:
     training_data = json.load(raw_training_data)
 
@@ -12,9 +16,16 @@ for tag, attr in training_data["tags"].items():
         continue
     
     attr["type"].update(unknowns_dict)
+    for v in types:
+        if v in attr['type']:
+            continue
+        else:
+            attr['type'][v] = 0
     attr["count"] += len(attr["type"])
     for t in attr["type"]:
         attr["type"][t] += 1
+    
+            
 
 with io.open('training-data-2_smoothed.json', 'w', encoding='utf-8-sig') as raw_training_data:
     json.dump(training_data, raw_training_data, ensure_ascii=False)
