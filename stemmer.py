7b2588a4a71780b0e45868c9eccdcf83731e9b5f
Tatag Aziz Prawiro <prawirotatag@gmail.com>
2019-03-07 13:46:32 +0700

    fixed stemmed test

--- stemmer.py
+++ stemmer.py
@@ -7,11 +7,17 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 punctuations = [".", ",","\\\"",":","(","/",")","-","?","'",";","&","--","...","!"]
-stem = False
+train_flag = True if input('stem training data?').lower() == 'y' else False
+stem = True
+
+FILE_IN = 'training-data.tsv' if train_flag else 'test-data.tsv'
+FILE_OUT = 'training-data-stemmed.tsv' if train_flag else 'test-data-stemmed.tsv'
+
+print(FILE_IN, FILE_OUT, train_flag)
 if stem:
-    with open('training-data.tsv') as training_data:
+    with open(FILE_IN) as training_data:
         reader = csv.reader(training_data, delimiter="\t")
-        with open('training-data-stemmed.tsv', 'wt', newline='') as stemmed_training_data:
+        with open(FILE_OUT, 'wt', newline='') as stemmed_training_data:
             writer = csv.writer(stemmed_training_data, delimiter="\t")
             for row in reader:
                 if len(row) == 0:
@@ -19,7 +25,10 @@ if stem:
                 else:
                     stemmed_word = row[0] if row[0] in punctuations else stemmer.stem(
                         row[0])
-                    writer.writerow([stemmed_word, row[1]])
+                    if train_flag:
+                        writer.writerow([stemmed_word, row[1]])
+                    else:
+                        writer.writerow([stemmed_word])
 else:
     with open('test-data_manual_tag.tsv') as manual_tag:
         reader = csv.reader(manual_tag, delimiter='\t')
